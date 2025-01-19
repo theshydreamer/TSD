ac35c0d6a824f4275223963b8183d5c407ae631c
jharrycreates <johnharrybeato@gmail.com>
2025-01-19 21:22:43 +0800

    function change

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("menu-toggle").addEventListener("click", () => {
-    const mobileMenu = document.getElementById("mobile-menu");
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  // Add click event to the menu toggle button
+  menuToggle.addEventListener("click", () => {
+    // Toggle the "hidden" class for the mobile menu
     mobileMenu.classList.toggle("hidden");
   });
 });
